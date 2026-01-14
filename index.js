a7889e5b6783786a9a0726e31ad4a3cccf70b273
Aayush Bhoj <aayushbhoj@gmail.com>
2026-01-15 01:10:58 +0530

    added save tab button, extension fully working now.

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
-let myLeads = [];
+
+let myLeads = []; 
+let oldLeads = [];
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage;
+    render(myLeads);
+}
+
+function render(leads){
+let listItems= ""
+for(let i=0;i<leads.length;i++){
+    listItems+= `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+`
+}
+ulEl.innerHTML = listItems;
+}
 
 inputBtn.addEventListener("click", function(){
     let lead = inputEl.value;
     myLeads.push(lead);
-    renderLead();
+    render(myLeads);
     inputEl.value="";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
 })
 
-function renderLead(){
-let listItem= `
-        <li>
-            <a target='_blank' href='${inputEl.value}'>
-                ${inputEl.value}
-            </a> </li>
-`
-ulEl.innerHTML += listItem
-}
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads=[]
+    render(myLeads)
+})
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        let lead = tabs[0].url
+        myLeads.push(lead)
+        render(myLeads)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    })
+    
+})
